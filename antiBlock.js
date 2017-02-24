7760bdfaa0ef05bc18434ff1c291e58543d4deef
KL44S <agustinbinci@hotmail.com>
2017-02-24 12:33:13 -0300

    Separado en dos lógicas: antibloqueo simple y profundo

--- antiBlock.js
+++ antiBlock.js
@@ -8,8 +8,7 @@
 		}
 	}
 
-	function unlock() {
-		var elements = document.getElementsByTagName('div');
+	function getElementsWidthGreaterZindex(elements) {
 		var frontElements = [];
 
 		for (var i = 0; i < elements.length; i++) {
@@ -35,14 +34,92 @@
 
 		}
 
-
-		frontElements.forEach(function(currentValue, index) {
-			currentValue.style.display = "none";
-		});
+		return frontElements;
 	}
 
-	window.onload = function() {
-		unlock();
+	function getElementsWithBackgroundOpacity(elements) {
+		var frontElements = [];
+
+		for (var i = 0; i < elements.length; i++) {
+			var opacity = getValueOfCssProperty(elements[i], "opacity");
+			var width = getValueOfCssProperty(elements[i], "width");
+			var height = getValueOfCssProperty(elements[i], "height");
+
+			if (opacity != undefined && opacity != null && opacity.length && !isNaN(opacity) && opacity < 1 && opacity > 0) {
+
+				if (width != undefined && width != null && width.length && width === "100%") {
+ 					
+ 					if (height != undefined && height != null && height.length && height === "100%") {
+ 						frontElements.push(elements[i]);
+					}
+
+				}
+			}
+		}
+
+		return frontElements;
+	}
+
+	function getElementsModalOrDialog(elements) {
+		var frontElements = [];
+
+		for (var i = 0; i < elements.length; i++) {
+			var cssClasses = elements[i].getAttribute("class");
+
+			if (cssClasses != null) {
+
+				cssClasses = cssClasses.toLowerCase();
+
+				if (cssClasses.includes("modal") || cssClasses.includes("dialog"))
+					frontElements.push(elements[i]);
+
+			}
+
+		}
+
+		return frontElements;
+	}
+
+	function classicUnlock(elements) {
+		var frontElements = getElementsModalOrDialog(elements);
+		frontElements = frontElements.concat(getElementsWithBackgroundOpacity(elements));
+
+		return frontElements;
+	}
+
+	function deepUnlock(elements) {
+		var frontElements = classicUnlock(elements);
+		frontElements = frontElements.concat(getElementsWidthGreaterZindex(elements));
+
+		return frontElements;
+	}
+
+	function unlock(unlockType) {
+		if (unlockType != undefined && unlockType != null) {
+			var elements = document.getElementsByTagName('div');
+			var frontElements = [];
+
+			unlockType = unlockType.toLowerCase();
+
+			switch(unlockType) {
+				case 'c':
+					frontElements = classicUnlock(elements);
+					break;
+
+				case 'd':
+					frontElements = classicUnlock(elements);
+					frontElements = deepUnlock(elements);
+					break;
+
+				default:
+					break;
+			}
+
+			frontElements.forEach(function(currentValue, index) {
+				currentValue.style.display = "none";
+			});			
+		}
+
 	}
 	
 }());
